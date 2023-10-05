118b2bb1f96a259ca85366d345eaac2c4660b4f2
Bemin <bemin_dawoud@yahoo.com>
2023-10-05 11:22:41 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: a c program that prints with put function
+ * return always 0 success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle") :
+	return (0) :
+}
