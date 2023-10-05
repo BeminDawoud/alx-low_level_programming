6a093c24ff20b081720afadab992333b71561add
Bemin <bemin_dawoud@yahoo.com>
2023-10-05 11:42:30 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message with proper grammar.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* Print the message with proper grammar using printf */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/* Return 0 to indicate successful execution */
+	return (0);
+}
+
