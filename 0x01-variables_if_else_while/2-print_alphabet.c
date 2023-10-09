ca37bdf55029c13e6e0e62f4f5c7830934c0e1b3
Bemin <bemin_dawoud@yahoo.com>
2023-10-08 19:24:24 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: last digit
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz"
+	int i = 0;
+
+	while (alphabet[i] != 0)
+	{
+	putchar(alphabet[i]);
+	i++;
+	}
+	return (0);
+}
