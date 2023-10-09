d218561c4b9f7ad4e9d7c2f335df04de2f41af3c
Bemin <bemin_dawoud@yahoo.com>
2023-10-08 19:58:43 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: last digit
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
