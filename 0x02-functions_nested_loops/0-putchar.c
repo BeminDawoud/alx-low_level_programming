2177de2f4766934df3cf3bb6d76875b8519ab50c
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 16:58:22 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	purchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
