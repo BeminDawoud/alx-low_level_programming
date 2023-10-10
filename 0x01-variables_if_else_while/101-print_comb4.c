ee72386f163b07af19ffb1a3d7eda0e15da5b7db
Bemin <bemin_dawoud@yahoo.com>
2023-10-09 19:42:09 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,6 @@ int main(void)
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
-
 				if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
 				{				
 					putchar(',');
@@ -31,8 +30,6 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
