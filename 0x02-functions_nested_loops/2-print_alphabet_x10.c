2ad47bf61360cd2e91f18f6c374fded4f55164ea
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 18:08:09 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Check description
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+			_putchar('\n');
+		}
+	}
+}
