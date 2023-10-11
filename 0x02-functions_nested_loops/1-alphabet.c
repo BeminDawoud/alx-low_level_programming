edadfd21b724a696a22a67cff8ba0c87b5e34ae7
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 17:18:48 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= z; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
