e4150a59accc5c420022cc156be0dafaece6a79d
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 17:52:56 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point of the program
- *
- * print_alphabet - description prints the alphabet in lowercase
- *
- * Return: 0 on success
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
 
 void print_alphabet(void) /* print_alphabet() it prints lowercase alphabet*/
