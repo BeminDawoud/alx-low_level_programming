cca3bade524dfc15ca0d29400fc059fc1f33540d
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 19:06:54 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,13 +9,16 @@
 
 int _isalpha(int c)
 {
-	if (c < 'a' || (c > 'z') {
+	if (c < 'a' || c > 'z')
+	{
 		return (0);
 	}
-	else if (c < 'A' || c > 'Z') {
+	else if (c < 'A' || c > 'Z')
+	{
 		return (0);
 	}
-	else {
+	else
+	{
 		return (1);
 	}
 
