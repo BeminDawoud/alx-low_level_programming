cd267c819819da4f6af832218d28143af8df85b7
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 18:41:39 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Check Description
+ * @c: parameter for function
+ *
+ * Description: function checks if c is a lowercase alphabet
+ * Return: 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c < 'a' || c > 'z' && c < 'A' || c > 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+
+}
