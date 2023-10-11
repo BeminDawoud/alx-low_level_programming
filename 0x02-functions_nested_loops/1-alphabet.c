2b2a5d7ed9d5952de01e7f6aafe0019e1b92cbd1
Bemin <bemin_dawoud@yahoo.com>
2023-10-10 17:46:16 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Description: print_alphabet prints the alphabet in lowercase fallowed by a new line
  *
  * Return: 0 on success
  */
