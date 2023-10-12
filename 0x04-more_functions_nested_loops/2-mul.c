8a3b7bff2805cf311d3edb3cd9a4567571a8d17e
Bemin <bemin_dawoud@yahoo.com>
2023-10-12 14:22:29 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - check Description
+ * @a: parameter
+ * @b: parameter
+ * Description: multiplies two numbers
+ *
+ * Return: 0 on success
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
