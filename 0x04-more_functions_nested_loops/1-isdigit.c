f43ff96683fc8b14fdc372899343047eeefe2038
Bemin <bemin_dawoud@yahoo.com>
2023-10-12 14:15:01 -0700

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - check Description
+ * @c: parameter
+ * Description: checks uppercase letters
+ *
+ * Return: 0 on success
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
