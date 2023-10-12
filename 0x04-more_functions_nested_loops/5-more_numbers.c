ba95dabe4e2b0daad93e64589056fa0a7804cd68
Bemin <bemin_dawoud@yahoo.com>
2023-10-12 15:23:44 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,18 @@
  *
  * Return: 0 on success
  */
-
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j = 0;
 
-	for (i = '0'; i <= '9'; i++)
+	for (; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j > 9)
-			{
-				_putchar(j / 10 + '0');
-			}
-			else
-				_putchar(j % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
