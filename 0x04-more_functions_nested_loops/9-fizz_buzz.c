925144eae271624bcf4113757eb5bda4afde0e19
Bemin <bemin_dawoud@yahoo.com>
2023-10-12 17:51:29 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int i;
 
+	printf("%d", 1);
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && !((i % 5) == 0))
-			printf("Fizz ");
+			printf(" Fizz");
 		else if ((i % 5) == 0 && !((i % 3) == 0))
-			printf("Buzz ");
+			printf(" Buzz");
 		else if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 	return (0);
