0eb7793757f90b4fb1e340e58a61ad6a71b82462
Bemin <bemin_dawoud@yahoo.com>
2023-10-16 19:42:02 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - a function that reverse a string
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = strlen(s) - 1;
+	int j = 0;
+	char *c;
+
+	while (i >= 0)
+	{
+		c[j] = s[i];
+		i--;
+		j++;
+	}
+	*s = *c;
+}
