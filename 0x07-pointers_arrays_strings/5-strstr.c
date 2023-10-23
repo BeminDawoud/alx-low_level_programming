920a59f8335a836210169537c4fba5105d5da4d7
Bemin <bemin_dawoud@yahoo.com>
2023-10-23 16:59:54 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,19 +8,26 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *temp = needle;
+	char *temp1 = needle;
+	char *temp2;
 
-	while (*haystack)
+	for (; *haystack ; haystack++)
 	{
-		while (*needle)
+		temp2 = haystack;
+
+		while (*haystack == *needle && *needle != '\0')
 		{
-			if (*haystack == *needle)
-				return (haystack);
 			needle++;
 			haystack++;
 		}
-		needle = temp;
-		haystack++;
+		if (*needle == '\0')
+			return (temp2);
+		else if (*needle != '\0')
+		{
+			needle = temp1;
+			continue;
+		}
+
 	}
 	return (NULL);
 }
