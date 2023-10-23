a2ecf396188f2abe0666f70dc9df65ed41ad0c16
Bemin <bemin_dawoud@yahoo.com>
2023-10-23 15:36:33 -0700

    a

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ * @s: string to search in
+ * @accept: characters to locate
+ * Return: returns pointer
+ */
+char *_strpbrk(char *s, char *accept);
+{
+	while (*s)
+	{
+		if (*s == accept)
+			return (s);
+		s++;
+	}
+	if (*s == accept)
+		return (s);
+	return (NULL);
+}
