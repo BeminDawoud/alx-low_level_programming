17b4ae2dd78cad60d4a07cdea4011a187cca7aed
Bemin <bemin_dawoud@yahoo.com>
2023-10-23 16:04:08 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - a function that searches a string for a substring.
+ * @haystack: string to search in
+ * @needle: string to locate
+ *
+ * Return: returns pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *temp = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack == *needle)
+				return (haystack);
+			needle++;
+			haystack++;
+		}
+		accept = temp;
+		s++;
+	}
+	return (NULL);
+}
