83740d53106464ae75eb9873116443d62a50eda7
Bemin <bemin_dawoud@yahoo.com>
2023-10-23 12:50:12 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
 		i++;
 	}
 	return (NULL);
