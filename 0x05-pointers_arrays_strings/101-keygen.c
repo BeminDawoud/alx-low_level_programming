1570450dbeefd75d70ec07b17bdc00aa1c16b8f8
Bemin <bemin_dawoud@yahoo.com>
2023-10-24 14:18:48 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - generating a random password
+ * Description: description
+ * Return: nothing for success
+*/
+
+int main(void)
+{
+	char password[12];
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbol[] = "!@#$^&*(_)@$&!";
+	char digits[] = "123456789";
+	int len1 = strlen(lower) - 1;
+	int len2 = strlen(upper) - 1;
+	int len3 = strlen(symbol) - 1;
+	int len4 = strlen(digits) - 1;
+
+	srand(time(NULL));
+	for (int i = 0; i < 12; i++)
+	{
+		int choice = rand() % 4;
+
+		if (choice == 0)
+			password[i] = lower[rand() % len1];
+		else if (choice == 1)
+			password[i] = upper[rand() % len2];
+		else if (choice == 2)
+			password[i] = symbol[rand() % len3];
+		else
+			password[i] = digits[rand() % len4];
+	}
+
+	password[12] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
