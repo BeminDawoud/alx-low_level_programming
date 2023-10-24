6cb65a74a9118618c138a4ee528b002e121187e3
Bemin <bemin_dawoud@yahoo.com>
2023-10-23 18:29:06 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string -  sets the value of a pointer to a char.
+ * @s: a pointer to a pointer of a string
+ * @to: a pointer to a string 
+ * Return: null
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
