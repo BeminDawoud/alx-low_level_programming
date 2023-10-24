f39ad2ccca4105b6f30eeb8a1845864a216c502a
Bemin <bemin_dawoud@yahoo.com>
2023-10-24 02:25:46 -0700

    a

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - the function encodes a string.
+ * @s: input string to encode
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	char check[] = "aAoOeElLtT";
+	char sub[] = "4400331177";
+	int i = 0;
+
+	for(; *s; s++)
+	{
+		for (; i < 9; i++)
+		{
+			if( *s == check[i])
+				*s = sub[i];
+		}
+		i = 0;
+	}
+	return (s);
+}
