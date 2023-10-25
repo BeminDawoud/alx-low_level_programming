9f1f951f390d5b90a21acab08bace628e203e561
Bemin <bemin_dawoud@yahoo.com>
2023-10-25 05:39:33 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts function with recursion
+ * @s: parameter
+ * Return: Void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	if (*s == '\0')
+		putchar('\n');
+}
