90b7a2e4cc3b2de7bd4e30dea288a829d1088d04
Bemin <bemin_dawoud@yahoo.com>
2023-10-25 12:50:45 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,9 @@
  */
 int is_prime_number(int n)
 {
-	if ((n % 10 == 1 || n % 10 == 3 || n % 10 == 7 || n % 10 == 9) && (n > 9 && n % 3 != 0))
+	if (n % 10 == 1 || n % 10 == 7)
+		return (1);
+	else if ((n % 10 == 3 || n % 10 == 9) && (n > 9 && n % 3 != 0))
 		return (1);
 	else
 		return (0);
