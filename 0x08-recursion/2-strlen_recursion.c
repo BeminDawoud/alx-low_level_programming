bf0a702a64305284b8665b621075fd66b368bb40
Bemin <bemin_dawoud@yahoo.com>
2023-10-25 10:27:18 -0700

    a

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen_recursion - puts function with recursion
+ * @s: parameter
+ * Return: Void
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+}
