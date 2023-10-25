05a8cc1017e2e345f7e7409678e827a52f194924
Bemin <bemin_dawoud@yahoo.com>
2023-10-25 11:04:43 -0700

    a

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * factorial - function with recursion
- * @n: Number to calculate factorial of.
+ * _pow_recursion - function with recursion
+ * @x: Number to raise it's power.
+ * @y: number to be raised by.
  * Return: Void
  */
 int _pow_recursion(int x, int y)
 {
 	if (y > 0)
-		return (x * _pow_recursion(x, (y-1)));
+		return (x * _pow_recursion(x, (y - 1)));
 	else if (y == 0)
 		return (1);
 	else
