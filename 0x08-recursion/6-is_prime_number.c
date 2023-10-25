8b0e105ef193e883f0ebf9fb523fe7b87372fba3
Bemin <bemin_dawoud@yahoo.com>
2023-10-25 12:52:58 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _is_prime_number - function with recursion
+ * is_prime_number - function with recursion
  * @n: the square we need to find the root for.
  * Return: number
  */
 int is_prime_number(int n)
 {
-	if (n % 10 == 1 || n % 10 == 7)
+	if ((n % 10 == 1 || n % 10 == 7) && (n > 10))
 		return (1);
 	else if ((n % 10 == 3 || n % 10 == 9) && (n > 9 && n % 3 != 0))
 		return (1);
