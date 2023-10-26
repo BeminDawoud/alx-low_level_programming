8df003d2b0ce7a569d37fc06ac91418c70a4ea04
Bemin <bemin_dawoud@yahoo.com>
2023-10-26 15:48:14 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,10 +6,20 @@
 *
 *Return: 0
 */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (s1 != s2)
+	if ((*s1 == '\0' || *s2 == '\0') && (*s1 == *s2 || *s2 == '*'))
+		return (1);
+	else if ((*s1 == '\0' || *s2 == '\0') && (*s1 != *s2 && *s2 != '*'))
 		return (0);
-	return (0);
+	else if (*s1 != *s2 && *s2 == '*' && *(s2 + 1) != '\0')
+		return (1);
+	else if (*s1 == *s2)
+		wildcmp(++s1, ++s2);
+	else if (*s1 != *s2 && *s2 == '*' && *(s2 + 1) != '\0')
+		wildcmp(s1, ++s2);
+	else if (*s1 != *s2 && *s2 != '*' && (*s1 != '\0' || *s2 != '\0'))
+		wildcmp(++s1, s2);
+	else
+		return (0);
 }
