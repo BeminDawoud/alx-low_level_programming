b38526ec4732777a2c4eaab17d268200acf57046
Bemin <bemin_dawoud@yahoo.com>
2023-10-27 16:23:15 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* main - Multiplying two numbers
+* @argc: count of args
+* @argv: array of strings
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 1;
+
+	if (argc < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+			sum = sum * atoi(argv[i]);
+		return (0);
+	}
+}
