910d85aed5d93532230ae8a7c79c11c1a4676848
Bemin <bemin_dawoud@yahoo.com>
2023-10-27 16:02:21 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - printing file name
+* @argc: count of args
+* @argv: array of strings
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	__attribute__((unused))char ***x = &argv;
+	printf("%d\n", argc);
+	return (0);
+}
