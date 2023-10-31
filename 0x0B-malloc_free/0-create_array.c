7706a012cf34b1224168299bb2b6bb7853dc8f02
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 10:49:41 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * create_array - create an array.
+ * @size: size of the array.
+ * @c: character.
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = (char *)malloc(sizeof(char) * size);
+	int i;
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	ptr[size] = '\0';
+	return (ptr);
+}
