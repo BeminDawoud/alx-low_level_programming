d5d57c958a3d56902403e8d3913aeb6dd2b44335
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 15:03:45 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,30 +9,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1 = 0, len2 = 0;
-	char *ptr;
+	int i, j, l1, l2;
+	char *newtxt1, *newtxt2;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	l1 = 0;
+	l2 = 0;
+	i = 0;
+	j = 0;
 	if (s1 == NULL)
-		s1 = NULL;
-
+		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
-
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-
-	ptr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (ptr == NULL)
+		s2 = "";
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+	newtxt1 = (char *) malloc(sizeof(char) * (l1 + l2 + 1));
+	newtxt2 = newtxt1;
+	if (newtxt1 == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < len2; j++)
-		ptr[i + j] = s2[j];
-
-	return (ptr);
+	while (i < l1)
+	{
+		newtxt1[i] = s1[i];
+		i++;
+	}
+	while (j <= l2 + 1)
+	{
+		newtxt1[i] = s2[j];
+		j++, i++;
+	}
+	return (newtxt2);
 }
