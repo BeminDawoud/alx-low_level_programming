4e58f5ee3a154c2ee1ef710f6dcebb4c96e9b98e
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 11:08:58 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 		for (i = 0; i < size; i++)
 			ptr[i] = c;
-	else if (size == 0 || ptr == NULL)
+	else
 		return (NULL);
-	ptr[size] = '\0';
 	return (ptr);
 }
