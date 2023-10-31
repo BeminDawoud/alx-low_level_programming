f2bf266b35c75d2001a27480dc9e79d28d11781c
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 11:50:32 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - duplicate an array.
+ * @str: string to copy.
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int len = strlen(str);
+	char *ptr = (char *)malloc(sizeof(char) * len);
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ptr[i] = str[i];
+	ptr[len] = '\0';
+	return (ptr);
+}
