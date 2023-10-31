0f89d6a93c0c7d34f63eaeff7fe4b857403ec294
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 16:54:40 -0700

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - concat a string.
+ * @width: width.
+ * @height: hieght.
+ * Return: pointer or NULL.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		ptr[i] = (int *)malloc(sizeof(int) * width);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+}
