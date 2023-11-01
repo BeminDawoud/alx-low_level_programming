59a78af6729dc6cd7f83ec78da6f2d2c48e8176c
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 18:56:15 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,30 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+* free_gridz - free allocated memory
+* @grid: allocated memory
+* @height: height
+* Return: void
+*/
+void free_gridz(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
 
 /**
- * alloc_grid - concat a string.
- * @width: width.
- * @height: hieght.
- * Return: pointer or NULL.
- */
+*alloc_grid - create array
+* @width: width
+* @height: height
+* Return: pointer or NULL
+*/
 int **alloc_grid(int width, int height)
 {
-	int **ptr, i, j;
+	int i, j;
+	int **ptr;
 
-	if (width <= 0 || height <= 0)
+	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	ptr = (int **)malloc(sizeof(int *) * height);
+	ptr = (int **) malloc(sizeof(int *) * height);
 
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = (int *)malloc(sizeof(int) * width);
+		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
 			return (NULL);
 	}
+	/*free_gridz(array, height);*/
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
