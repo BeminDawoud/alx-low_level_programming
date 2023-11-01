06dec530d7a2f61f13ecaf4fb66b7ff986dfaae3
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 18:57:35 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,50 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-/**
-* free_gridz - free allocated memory
-* @grid: allocated memory
-* @height: height
-* Return: void
-*/
-void free_gridz(int **grid, int height)
-{
-	int i;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+/**
+* free_gridk - free a grid
+* @grid: grid
+* @height: height grid
+*/
+void free_gridk(int **grid, int height)
+{
+	int pos = 0;
+
+	while (pos < height)
+	{
+		free(grid[pos]);
+		pos++;
+	}
 	free(grid);
 }
 
 /**
-*alloc_grid - create array
-* @width: width
-* @height: height
-* Return: pointer or NULL
+*alloc_grid - create empty array
+* @width: width array
+* @height: height array
+* Return: array
 */
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **ptr;
+	int **grid;
 
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	ptr = (int **) malloc(sizeof(int *) * height);
+	grid = (int **) malloc(sizeof(int *) * height);
 
-	if (ptr == NULL)
+	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(sizeof(int) * width);
-		if (ptr[i] == NULL)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 			return (NULL);
 	}
-	/*free_gridz(array, height);*/
+	/*free_gridk(array, height);*/
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			ptr[i][j] = 0;
+			grid[i][j] = 0;
 	}
-	return (ptr);
+	return (grid);
 }
