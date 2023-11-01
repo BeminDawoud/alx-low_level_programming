8ce657667624d68ef8afd7e94065580721e36204
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 17:45:41 -0700

    a

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* argstostr - dividing words
+* @ac: integer
+* @av: string
+* Return: string or NULL
+*/
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	return (NULL);
+}
