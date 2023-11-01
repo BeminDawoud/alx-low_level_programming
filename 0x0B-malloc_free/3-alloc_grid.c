3aa6acd4a8c0ecf5a53a37093496d96dcc06b57e
Bemin <bemin_dawoud@yahoo.com>
2023-10-31 19:03:11 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,12 @@ int **alloc_grid(int width, int height)
 	{
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
