49da6ad57ceda1ea3f0dd1ab08de44f00eb3ee9e
Bemin <bemin_dawoud@yahoo.com>
2023-11-06 16:41:49 -0800

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,17 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
