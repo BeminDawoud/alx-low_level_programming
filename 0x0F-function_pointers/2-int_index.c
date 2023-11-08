39f52d89c791bd3b8986b2fc1ae417df2a54feeb
Bemin <bemin_dawoud@yahoo.com>
2023-11-08 08:45:23 -0800

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+*int_index -  a function that searches for an integer
+*@array: array
+*@size: array size
+*@cmp: function pointer
+*Return: Index or -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+	if (cmp && array)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
