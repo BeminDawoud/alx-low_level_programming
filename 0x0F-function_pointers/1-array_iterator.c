c05af966f994807d105e15eac6d2f880c584c574
Bemin <bemin_dawoud@yahoo.com>
2023-11-08 08:12:36 -0800

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 /**
 *array_iterator - function to call a function
-*@array: array 
+*@array: array
 *@size: array size
 *@action: function pointer
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (action)
+	if (action && array)
 	{
 		size_t i;
 
