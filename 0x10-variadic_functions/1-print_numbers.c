131b8ce76b3c0909c8db7d19f265a4ec07651856
Bemin <bemin_dawoud@yahoo.com>
2023-11-09 14:09:34 -0800

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		separator = "";
 	}
 	if (n == 0)
-		return;
+		return (NULL);
 	va_start(args, n);
 
 	num = va_arg(args, int);
