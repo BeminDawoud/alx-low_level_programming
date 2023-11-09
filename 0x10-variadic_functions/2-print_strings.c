2606379f051dd61e41af7b11b48e01015b1d93ad
Bemin <bemin_dawoud@yahoo.com>
2023-11-09 14:36:14 -0800

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,10 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator && i != 0)
 			printf("%s", separator);
 		str = va_arg(args, char *);
-		if (str == NULL)
-			printf("nil");
-		else
+		if (str != NULL)
 			printf("%s", str);
+		else
+			printf("nil");
 	}
 	putchar('\n');
 	va_end(args);
