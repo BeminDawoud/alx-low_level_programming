d9893be75418e07e2b1b798ee427764138982129
Bemin <bemin_dawoud@yahoo.com>
2023-11-16 09:18:58 -0800

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,19 +2,19 @@
 /**
  * add_node - a function to print element of list.
  * @head: pointer to the first node.
- * str: string to be added. 
+ * @str: string to be added.
  * Return: number of elements.
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t count = 0;
-	const list_t *ptr;
+	list_t *ptr;
 
-	ptr = malloc(sizeof(const list_t));
+	ptr = malloc(sizeof(list_t));
 	if (ptr == NULL)
 		return (NULL);
 	ptr->str = strdup(str);
-	ptr->next = head;
-	head = ptr;
-	return (head);
+	ptr->len = strlen(str);
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
 }
