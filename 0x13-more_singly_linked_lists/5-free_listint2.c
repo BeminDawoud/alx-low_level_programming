a790c256be2117adb161261e743aa984305c2844
Bemin <bemin_dawoud@yahoo.com>
2023-11-20 12:59:45 -0800

    update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function to print element of list.
+ * @head: pointer to the first node.
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
