404e36c14e015d5c7a0fdb61e9c938cd8474b529
Bemin <bemin_dawoud@yahoo.com>
2023-11-20 14:59:13 -0800

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	node = malloc(sizeof(listint_t));
 	node->n = n;
-	node->next = temp->next->next;
+	node->next = temp->next;
 	temp->next = node;
 	return (node);
 
