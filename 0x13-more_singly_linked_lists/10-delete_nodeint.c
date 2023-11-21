3ee754c9b4229ff74afc16e111aeec14ecb219e4
Bemin <bemin_dawoud@yahoo.com>
2023-11-21 13:11:03 -0800

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	previous = *head;
 	current = *head;
-	if (index == 0)
+	if (*head && index == 0)
 	{
 		*head = current->next;
 		free(current);
