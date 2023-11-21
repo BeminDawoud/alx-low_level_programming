e9e2bb35db29dbfb206d1704da5ac13fc1b8b602
Bemin <bemin_dawoud@yahoo.com>
2023-11-21 14:46:36 -0800

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,20 +3,29 @@
  * print_listint_safe - a function to print element of list.
  * @head: pointer tio the first node.
  * Return: number of elements.
-*/
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *ptr;
+	size_t count = 0, i;
+	const listint_t *ptr, *check;
 
-	if (head == NULL)
-		return (98);
 	ptr = head;
 	while (ptr)
 	{
-		printf("%d\n", ptr->n);
+		printf("[%p] %d\n", (void *)ptr, ptr->n);
 		ptr = ptr->next;
 		count++;
+		check = head;
+		i = 0;
+		for (i = 0; i < count; i++)
+		{
+			if (ptr == check)
+			{
+				printf("-> [%p] %d\n", (void *)ptr, ptr->n);
+				return count;
+			}
+			check = check->next;
+		}
 	}
 	return (count);
 }
