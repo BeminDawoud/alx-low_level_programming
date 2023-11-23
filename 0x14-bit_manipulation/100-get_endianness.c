5c373fe4959f1fa29f84d772c910bbf28f152771
Bemin <bemin_dawoud@yahoo.com>
2023-11-23 14:17:06 -0800

    update

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * et_endianness - check endianness.
+ * Return:  1 or 0.
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 0;
+	char *ptr = (char *)&num;
+
+	if (*ptr)
+		return (1);
+	else
+		return (0);
+}
