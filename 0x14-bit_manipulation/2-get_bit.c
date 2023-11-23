28fe8e8c4bfc2c25159cfeddc81818fbd437970a
Bemin <bemin_dawoud@yahoo.com>
2023-11-23 12:35:17 -0800

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,11 +2,16 @@
 /**
  * get_bit - function to print binary representation.
  * @n: number in decimal.
+ * @index: the index.
  * Return: the bit.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int index_max = sizeof(unsigned long int) * 8;
+
+	if(index > index_max)
+		return(-1);
 	if (((n >> index) & 1) == 1)
 		return (1);
 	else
