cae01f1c9443ae2a0c7a53b09222a9cb5e82bdd6
Bemin <bemin_dawoud@yahoo.com>
2023-11-23 12:38:58 -0800

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int index_max = sizeof(unsigned long int) * 8;
 
-	if(index > index_max)
-		return(-1);
+	if (index > index_max)
+		returni (-1);
 	if (((n >> index) & 1) == 1)
 		return (1);
 	else
