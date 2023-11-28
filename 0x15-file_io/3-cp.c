13d90273b28941add95faa82b7c77dfd16f79aae
Bemin <bemin_dawoud@yahoo.com>
2023-11-27 18:07:52 -0800

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,46 +5,54 @@
  * @argv: argument string.
  * Return: .
  */
+
 int main(int argc, char **argv)
 {
-	int fd_read, fd_write, len;
-	ssize_t helper;
-	char buffer[1024];
+	int fd_read, fd_write;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	fd_read = open(argv[1], O_RDONLY);
 	if (fd_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_write = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_write == -1)
 	{
 		close(fd_read);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	helper = read(fd_read, buffer, 1024);
-	if (helper == -1)
+	while ((bytes_read = read(fd_read, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_write, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			close(fd_read);
+			close(fd_write);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
 	{
 		close(fd_read);
 		close(fd_write);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	len = strlen(buffer);
-	helper = write(fd_read, buffer, len);
-	if (helper == -1)
+	if (close(fd_read) == -1 || close(fd_write) == -1)
 	{
-		close(fd_read);
-		close(fd_write);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(2, "Error: Can't close fd %d\n", fd_read);
+		exit(100);
 	}
 	return (0);
 }
