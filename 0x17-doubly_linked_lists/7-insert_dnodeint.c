d5e9f968849f3de0ac91e273c2a6f870df68717e
Bemin <bemin_dawoud@yahoo.com>
2024-01-11 16:13:49 -0800

    adding function to 7.insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,3 +46,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 		return (NULL);
 }
+
+/**
+ * dlistint_len - function to return length of a doubly linked list
+ * @h: pointer to the head of the list
+ * Return: number of elements in linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
