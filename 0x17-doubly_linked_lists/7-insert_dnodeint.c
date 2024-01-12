f23f41cad2f6ad1b41123b5b99758ccae774abac
Bemin <bemin_dawoud@yahoo.com>
2024-01-11 16:29:36 -0800

    updating

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 			count++;
 		}
+		if (temp == NULL)
+			return (NULL);
 		newNode->prev = temp->prev;
 		newNode->next = temp;
 		temp->prev->next = newNode;
